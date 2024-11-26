216cc9f84c60cdd573b12f39670ea900343ff255
23jn0204 <23jn0204@jec.ac.jp>
2024-11-26 09:51:22 +0900

    商品管理画面完成

--- 卒業制作/frmGoods.Designer.cs
+++ 卒業制作/frmGoods.Designer.cs
@@ -30,35 +30,157 @@
         {
             this.txtGoods = new System.Windows.Forms.TextBox();
             this.btnSerch = new System.Windows.Forms.Button();
+            this.lstCategory = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgvGoods = new System.Windows.Forms.DataGridView();
+            this.goods_code = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.goods_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGoods)).BeginInit();
             this.SuspendLayout();
             // 
             // txtGoods
             // 
-            this.txtGoods.Location = new System.Drawing.Point(107, 12);
+            this.txtGoods.Location = new System.Drawing.Point(146, 15);
             this.txtGoods.Name = "txtGoods";
             this.txtGoods.Size = new System.Drawing.Size(211, 28);
             this.txtGoods.TabIndex = 0;
             // 
             // btnSerch
             // 
-            this.btnSerch.Location = new System.Drawing.Point(351, 12);
+            this.btnSerch.Location = new System.Drawing.Point(363, 12);
             this.btnSerch.Name = "btnSerch";
-            this.btnSerch.Size = new System.Drawing.Size(94, 36);
+            this.btnSerch.Size = new System.Drawing.Size(94, 34);
             this.btnSerch.TabIndex = 1;
             this.btnSerch.Text = "検索";
             this.btnSerch.UseVisualStyleBackColor = true;
+            // 
+            // lstCategory
+            // 
+            this.lstCategory.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.lstCategory.FormattingEnabled = true;
+            this.lstCategory.Location = new System.Drawing.Point(569, 16);
+            this.lstCategory.Name = "lstCategory";
+            this.lstCategory.Size = new System.Drawing.Size(145, 29);
+            this.lstCategory.TabIndex = 2;
+            this.lstCategory.SelectedIndexChanged += new System.EventHandler(this.lstCategory_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(473, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "カテゴリ別";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 21);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "商品名";
+            // 
+            // dgvGoods
+            // 
+            this.dgvGoods.AllowUserToAddRows = false;
+            this.dgvGoods.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgvGoods.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvGoods.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvGoods.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.goods_code,
+            this.goods_name,
+            this.price});
+            this.dgvGoods.Location = new System.Drawing.Point(48, 72);
+            this.dgvGoods.Name = "dgvGoods";
+            this.dgvGoods.RowTemplate.Height = 21;
+            this.dgvGoods.Size = new System.Drawing.Size(666, 273);
+            this.dgvGoods.TabIndex = 5;
+            // 
+            // goods_code
+            // 
+            this.goods_code.DataPropertyName = "Goods_code";
+            this.goods_code.HeaderText = "商品番号";
+            this.goods_code.Name = "goods_code";
+            this.goods_code.Width = 90;
+            // 
+            // goods_name
+            // 
+            this.goods_name.DataPropertyName = "Goods_name";
+            this.goods_name.HeaderText = "商品名";
+            this.goods_name.Name = "goods_name";
+            this.goods_name.Width = 90;
+            // 
+            // price
+            // 
+            this.price.DataPropertyName = "price";
+            this.price.HeaderText = "価格";
+            this.price.Name = "price";
+            this.price.Width = 71;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(48, 366);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(154, 33);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "新規商品追加";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(228, 366);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(199, 33);
+            this.btnEdit.TabIndex = 7;
+            this.btnEdit.Text = "選択した商品の編集";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(461, 366);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(203, 33);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "選択した商品の削除";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(607, 426);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(128, 33);
+            this.btnClose.TabIndex = 9;
+            this.btnClose.Text = "閉じる";
+            this.btnClose.UseVisualStyleBackColor = true;
             // 
             // frmGoods
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(665, 370);
+            this.ClientSize = new System.Drawing.Size(768, 480);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dgvGoods);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstCategory);
             this.Controls.Add(this.btnSerch);
             this.Controls.Add(this.txtGoods);
             this.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.Name = "frmGoods";
             this.Text = "商品管理画面";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGoods)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -68,5 +190,16 @@
 
         private System.Windows.Forms.TextBox txtGoods;
         private System.Windows.Forms.Button btnSerch;
+        private System.Windows.Forms.ComboBox lstCategory;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dgvGoods;
+        private System.Windows.Forms.DataGridViewTextBoxColumn goods_code;
+        private System.Windows.Forms.DataGridViewTextBoxColumn goods_name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn price;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnClose;
     }
 }
