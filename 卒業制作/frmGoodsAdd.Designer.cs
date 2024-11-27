d9611dc1fa7857f7ca84a7ce68ef62ba18aed5f7
23JN0220 <23JN0220@jec.ac.jp>
2024-11-27 12:21:25 +0900

    新規商品追加画面を追加

---
+++ 卒業制作/frmGoodsAdd.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace 卒業制作
+{
+    partial class frmGoodsAdd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnOs = new System.Windows.Forms.Button();
+            this.btnFan = new System.Windows.Forms.Button();
+            this.btnCase = new System.Windows.Forms.Button();
+            this.btnPowerUnit = new System.Windows.Forms.Button();
+            this.btnHdd = new System.Windows.Forms.Button();
+            this.btnSsd = new System.Windows.Forms.Button();
+            this.btnGpu = new System.Windows.Forms.Button();
+            this.btnMemory = new System.Windows.Forms.Button();
+            this.btnMotherboard = new System.Windows.Forms.Button();
+            this.btnCooler = new System.Windows.Forms.Button();
+            this.btnCpu = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(534, 104);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(168, 30);
+            this.btnBack.TabIndex = 23;
+            this.btnBack.Text = "戻る";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // btnOs
+            // 
+            this.btnOs.Location = new System.Drawing.Point(360, 104);
+            this.btnOs.Name = "btnOs";
+            this.btnOs.Size = new System.Drawing.Size(168, 30);
+            this.btnOs.TabIndex = 22;
+            this.btnOs.Text = "OS";
+            this.btnOs.UseVisualStyleBackColor = true;
+            // 
+            // btnFan
+            // 
+            this.btnFan.Location = new System.Drawing.Point(186, 101);
+            this.btnFan.Name = "btnFan";
+            this.btnFan.Size = new System.Drawing.Size(168, 33);
+            this.btnFan.TabIndex = 21;
+            this.btnFan.Text = "ケースファン";
+            this.btnFan.UseVisualStyleBackColor = true;
+            // 
+            // btnCase
+            // 
+            this.btnCase.Location = new System.Drawing.Point(12, 101);
+            this.btnCase.Name = "btnCase";
+            this.btnCase.Size = new System.Drawing.Size(168, 33);
+            this.btnCase.TabIndex = 20;
+            this.btnCase.Text = "PCケース";
+            this.btnCase.UseVisualStyleBackColor = true;
+            // 
+            // btnPowerUnit
+            // 
+            this.btnPowerUnit.Location = new System.Drawing.Point(534, 67);
+            this.btnPowerUnit.Name = "btnPowerUnit";
+            this.btnPowerUnit.Size = new System.Drawing.Size(168, 28);
+            this.btnPowerUnit.TabIndex = 19;
+            this.btnPowerUnit.Text = "電源ユニット";
+            this.btnPowerUnit.UseVisualStyleBackColor = true;
+            // 
+            // btnHdd
+            // 
+            this.btnHdd.Location = new System.Drawing.Point(360, 69);
+            this.btnHdd.Name = "btnHdd";
+            this.btnHdd.Size = new System.Drawing.Size(168, 26);
+            this.btnHdd.TabIndex = 18;
+            this.btnHdd.Text = "HDD";
+            this.btnHdd.UseVisualStyleBackColor = true;
+            // 
+            // btnSsd
+            // 
+            this.btnSsd.Location = new System.Drawing.Point(186, 69);
+            this.btnSsd.Name = "btnSsd";
+            this.btnSsd.Size = new System.Drawing.Size(168, 26);
+            this.btnSsd.TabIndex = 17;
+            this.btnSsd.Text = "SSD";
+            this.btnSsd.UseVisualStyleBackColor = true;
+            // 
+            // btnGpu
+            // 
+            this.btnGpu.Location = new System.Drawing.Point(12, 67);
+            this.btnGpu.Name = "btnGpu";
+            this.btnGpu.Size = new System.Drawing.Size(168, 28);
+            this.btnGpu.TabIndex = 16;
+            this.btnGpu.Text = "グラフィックボード";
+            this.btnGpu.UseVisualStyleBackColor = true;
+            // 
+            // btnMemory
+            // 
+            this.btnMemory.Location = new System.Drawing.Point(534, 33);
+            this.btnMemory.Name = "btnMemory";
+            this.btnMemory.Size = new System.Drawing.Size(168, 28);
+            this.btnMemory.TabIndex = 15;
+            this.btnMemory.Text = "メモリ";
+            this.btnMemory.UseVisualStyleBackColor = true;
+            // 
+            // btnMotherboard
+            // 
+            this.btnMotherboard.Location = new System.Drawing.Point(360, 33);
+            this.btnMotherboard.Name = "btnMotherboard";
+            this.btnMotherboard.Size = new System.Drawing.Size(168, 28);
+            this.btnMotherboard.TabIndex = 14;
+            this.btnMotherboard.Text = "マザーボード";
+            this.btnMotherboard.UseVisualStyleBackColor = true;
+            // 
+            // btnCooler
+            // 
+            this.btnCooler.Location = new System.Drawing.Point(186, 33);
+            this.btnCooler.Name = "btnCooler";
+            this.btnCooler.Size = new System.Drawing.Size(168, 28);
+            this.btnCooler.TabIndex = 13;
+            this.btnCooler.Text = "CPUクーラー";
+            this.btnCooler.UseVisualStyleBackColor = true;
+            // 
+            // btnCpu
+            // 
+            this.btnCpu.Location = new System.Drawing.Point(12, 33);
+            this.btnCpu.Name = "btnCpu";
+            this.btnCpu.Size = new System.Drawing.Size(168, 28);
+            this.btnCpu.TabIndex = 12;
+            this.btnCpu.Text = "CPU";
+            this.btnCpu.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(369, 21);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "追加する商品のカテゴリーを選択してください";
+            // 
+            // frmGoodsAdd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 149);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnOs);
+            this.Controls.Add(this.btnFan);
+            this.Controls.Add(this.btnCase);
+            this.Controls.Add(this.btnPowerUnit);
+            this.Controls.Add(this.btnHdd);
+            this.Controls.Add(this.btnSsd);
+            this.Controls.Add(this.btnGpu);
+            this.Controls.Add(this.btnMemory);
+            this.Controls.Add(this.btnMotherboard);
+            this.Controls.Add(this.btnCooler);
+            this.Controls.Add(this.btnCpu);
+            this.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "frmGoodsAdd";
+            this.Text = "新規商品追加";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnOs;
+        private System.Windows.Forms.Button btnFan;
+        private System.Windows.Forms.Button btnCase;
+        private System.Windows.Forms.Button btnPowerUnit;
+        private System.Windows.Forms.Button btnHdd;
+        private System.Windows.Forms.Button btnSsd;
+        private System.Windows.Forms.Button btnGpu;
+        private System.Windows.Forms.Button btnMemory;
+        private System.Windows.Forms.Button btnMotherboard;
+        private System.Windows.Forms.Button btnCooler;
+        private System.Windows.Forms.Button btnCpu;
+        private System.Windows.Forms.Label label1;
+    }
+}
