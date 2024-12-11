9cbd8d39ec7727e11b077fb4b60f1b451771cb18
23JN0220 <23JN0220@jec.ac.jp>
2024-12-11 11:24:53 +0900

    複数項目選択画面のデザイン完了

--- 卒業制作/frmGoodsGokansei.Designer.cs
+++ 卒業制作/frmGoodsGokansei.Designer.cs
@@ -28,21 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.btnOK = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(482, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "対応する項目にチェックを入れて「OK」をクリックしてください";
+            // 
+            // dgvData
+            // 
+            this.dgvData.AllowUserToAddRows = false;
+            this.dgvData.AllowUserToDeleteRows = false;
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Location = new System.Drawing.Point(16, 33);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.RowTemplate.Height = 21;
+            this.dgvData.Size = new System.Drawing.Size(742, 396);
+            this.dgvData.TabIndex = 1;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(316, 446);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(140, 27);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
             // 
             // frmGoodsGokansei
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1467, 788);
+            this.ClientSize = new System.Drawing.Size(770, 485);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.dgvData);
+            this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.Name = "frmGoodsGokansei";
-            this.Text = "Form1";
+            this.Text = "項目選択";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.Button btnOK;
     }
 }
