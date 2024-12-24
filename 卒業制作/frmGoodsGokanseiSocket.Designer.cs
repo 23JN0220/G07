167f70b1c2695f2e80dadd9636ff42cc92e75682
23JN0220 <23jn0220@jec.ac.jp>
2024-12-24 11:36:34 +0900

    ソケット選択画面追加

---
+++ 卒業制作/frmGoodsGokanseiSocket.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace 卒業制作
+{
+    partial class frmGoodsGokanseiSocket
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstData = new System.Windows.Forms.ListBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(549, 21);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "(Ctrlキーを押しなががら項目をクリックすることで複数選択できます)";
+            // 
+            // lstData
+            // 
+            this.lstData.FormattingEnabled = true;
+            this.lstData.ItemHeight = 21;
+            this.lstData.Location = new System.Drawing.Point(16, 54);
+            this.lstData.Name = "lstData";
+            this.lstData.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lstData.Size = new System.Drawing.Size(534, 361);
+            this.lstData.TabIndex = 9;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(475, 439);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 34);
+            this.btnClose.TabIndex = 8;
+            this.btnClose.Text = "閉じる";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(211, 439);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(140, 34);
+            this.btnOK.TabIndex = 7;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(427, 21);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "対応する項目を選択して「OK」をクリックしてください";
+            // 
+            // frmGoodsGokanseiSocket
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 485);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lstData);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "frmGoodsGokanseiSocket";
+            this.Text = "項目選択";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstData;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label label1;
+    }
+}
