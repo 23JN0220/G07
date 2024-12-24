87aca318d1e31987f960bc0711d09803912f6c0d
23JN0220 <23jn0220@jec.ac.jp>
2024-12-24 16:20:26 +0900

    ケースファン関係の開発準備

---
+++ ClassLibrary/GoodsFan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class GoodsFan
+    {
+        public int goods_code { get; set; }
+        public int size_id { get; set; }
+        public int quantity { get; set; }
+    }
+}
