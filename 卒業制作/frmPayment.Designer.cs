59303c50b152562022e1553a1db155b643131c1d
23JN0220 <23jn0220@jec.ac.jp>
2025-01-30 14:43:02 +0900

    クレジットカード番号が表示されるのはまずいので修正したぜ

--- 卒業制作/frmPayment.Designer.cs
+++ 卒業制作/frmPayment.Designer.cs
@@ -33,6 +33,9 @@
             this.btnSearch = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
             this.dgvPayment = new System.Windows.Forms.DataGridView();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnSearchReset = new System.Windows.Forms.Button();
+            this.btnDetail = new System.Windows.Forms.Button();
             this.order_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.order_date = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.member_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -40,9 +43,6 @@
             this.creditcard_number = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.convenience_store = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.paymented = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.btnClose = new System.Windows.Forms.Button();
-            this.btnSearchReset = new System.Windows.Forms.Button();
-            this.btnDetail = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPayment)).BeginInit();
             this.SuspendLayout();
             // 
@@ -106,6 +106,36 @@
             this.dgvPayment.Size = new System.Drawing.Size(751, 283);
             this.dgvPayment.TabIndex = 4;
             // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(663, 395);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(136, 37);
+            this.btnClose.TabIndex = 7;
+            this.btnClose.Text = "閉じる";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnSearchReset
+            // 
+            this.btnSearchReset.Location = new System.Drawing.Point(558, 18);
+            this.btnSearchReset.Name = "btnSearchReset";
+            this.btnSearchReset.Size = new System.Drawing.Size(122, 28);
+            this.btnSearchReset.TabIndex = 3;
+            this.btnSearchReset.Text = "検索リセット";
+            this.btnSearchReset.UseVisualStyleBackColor = true;
+            this.btnSearchReset.Click += new System.EventHandler(this.btnSearchReset_Click);
+            // 
+            // btnDetail
+            // 
+            this.btnDetail.Location = new System.Drawing.Point(48, 395);
+            this.btnDetail.Name = "btnDetail";
+            this.btnDetail.Size = new System.Drawing.Size(262, 37);
+            this.btnDetail.TabIndex = 6;
+            this.btnDetail.Text = "選択された番号の明細";
+            this.btnDetail.UseVisualStyleBackColor = true;
+            this.btnDetail.Click += new System.EventHandler(this.btnDetail_Click);
+            // 
             // order_id
             // 
             this.order_id.DataPropertyName = "order_id";
@@ -144,6 +174,7 @@
             this.creditcard_number.HeaderText = "カード番号";
             this.creditcard_number.Name = "creditcard_number";
             this.creditcard_number.ReadOnly = true;
+            this.creditcard_number.Visible = false;
             this.creditcard_number.Width = 124;
             // 
             // convenience_store
@@ -161,36 +192,6 @@
             this.paymented.Name = "paymented";
             this.paymented.ReadOnly = true;
             this.paymented.Width = 134;
-            // 
-            // btnClose
-            // 
-            this.btnClose.Location = new System.Drawing.Point(663, 395);
-            this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(136, 37);
-            this.btnClose.TabIndex = 7;
-            this.btnClose.Text = "閉じる";
-            this.btnClose.UseVisualStyleBackColor = true;
-            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
-            // 
-            // btnSearchReset
-            // 
-            this.btnSearchReset.Location = new System.Drawing.Point(558, 18);
-            this.btnSearchReset.Name = "btnSearchReset";
-            this.btnSearchReset.Size = new System.Drawing.Size(122, 28);
-            this.btnSearchReset.TabIndex = 3;
-            this.btnSearchReset.Text = "検索リセット";
-            this.btnSearchReset.UseVisualStyleBackColor = true;
-            this.btnSearchReset.Click += new System.EventHandler(this.btnSearchReset_Click);
-            // 
-            // btnDetail
-            // 
-            this.btnDetail.Location = new System.Drawing.Point(48, 395);
-            this.btnDetail.Name = "btnDetail";
-            this.btnDetail.Size = new System.Drawing.Size(262, 37);
-            this.btnDetail.TabIndex = 6;
-            this.btnDetail.Text = "選択された番号の明細";
-            this.btnDetail.UseVisualStyleBackColor = true;
-            this.btnDetail.Click += new System.EventHandler(this.btnDetail_Click);
             // 
             // frmPayment
             // 
@@ -223,6 +224,9 @@
         private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.Button btnDelete;
         private System.Windows.Forms.DataGridView dgvPayment;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnSearchReset;
+        private System.Windows.Forms.Button btnDetail;
         private System.Windows.Forms.DataGridViewTextBoxColumn order_id;
         private System.Windows.Forms.DataGridViewTextBoxColumn order_date;
         private System.Windows.Forms.DataGridViewTextBoxColumn member_id;
@@ -230,8 +234,5 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn creditcard_number;
         private System.Windows.Forms.DataGridViewTextBoxColumn convenience_store;
         private System.Windows.Forms.DataGridViewTextBoxColumn paymented;
-        private System.Windows.Forms.Button btnClose;
-        private System.Windows.Forms.Button btnSearchReset;
-        private System.Windows.Forms.Button btnDetail;
     }
 }
